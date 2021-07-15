0dffd791ba474ea8b0b1603baa3ee291138a6805
konjacdesert5289 <56007373+konjacdesert5289@users.noreply.github.com>
2021-07-15 09:25:14 +0900

    命名規則を変更

--- src/canvasscreen.ts
+++ src/canvasscreen.ts
@@ -1,41 +1,50 @@
 export class CanvasScreen {
-  private display: CanvasRenderingContext2D;
-  private buffer: CanvasRenderingContext2D;
-  constructor(width: number, height: number, context: CanvasRenderingContext2D) {
+  private _display: CanvasRenderingContext2D;
+  private _buffer: CanvasRenderingContext2D;
+
+  public get Display(): CanvasRenderingContext2D {
+    return this._display;
+  }
+  public get Buffer(): CanvasRenderingContext2D {
+    return this._buffer;
+  }
+
+  constructor(width: number, height: number, context: CanvasRenderingContext2D, dispWidth = 0, dispHeight = 0) {
     // コピーor生成
-    this.display = context;
+    this._display = context;
 
     // チェック
     width = width <= 0 ? 1 : width;
     height = height <= 0 ? 1 : height;
+    dispWidth = dispWidth <= 0 ? width : dispWidth;
+    dispHeight = dispHeight <= 0 ? height : dispHeight;
 
     // 設定
-    this.display.canvas.width = width;
-    this.display.canvas.height = height;
+    this._display.canvas.width = dispWidth;
+    this._display.canvas.height = dispHeight;
+    this._display.imageSmoothingEnabled = false;
+    this._display.scale(dispWidth / width, dispHeight / height);
 
     // バッファ
-    this.buffer = document.createElement("canvas").getContext("2d");
-    this.buffer.canvas.width = width;
-    this.buffer.canvas.height = height;
-    this.buffer.imageSmoothingEnabled = false;
+    this._buffer = document.createElement("canvas").getContext("2d");
+    this._buffer.canvas.width = width;
+    this._buffer.canvas.height = height;
+    this._buffer.imageSmoothingEnabled = false;
 
     // きれいにしておこうね
     this.clearBuffer();
     this.updateDisp();
   }
-  public get(select: "buffer" | "display"): CanvasRenderingContext2D {
-    return select == "buffer" ? this.buffer : this.display;
-  }
 
   public clearBuffer(): void {
-    this.buffer.resetTransform();
-    if (this.buffer.globalAlpha != 1) this.buffer.globalAlpha = 1;
-    this.buffer.fillStyle = "#fff";
-    this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
+    this._buffer.resetTransform();
+    if (this._buffer.globalAlpha != 1) this._buffer.globalAlpha = 1;
+    this._buffer.fillStyle = "#000";
+    this._buffer.fillRect(0, 0, this._buffer.canvas.width, this._buffer.canvas.height);
   }
   public updateDisp(): void {
     // this.display.resetTransform();
-    this.display.drawImage(this.buffer.canvas, 0, 0);
+    this._display.drawImage(this._buffer.canvas, 0, 0);
     // this.clearBuffer();
   }
 }
--- src/game.ts
+++ src/game.ts
@@ -4,15 +4,21 @@ import { InputAIO } from "./input";
 
 export class Game {
   public static Info = {
-    Width: 240,
-    Height: 320,
+    Width: 256,
+    Height: 240,
+    DispWidth: 0,
   } as const;
   private _canvasScreen: CanvasScreen;
   private _input: InputAIO;
   private _deltaTime: DeltaTime;
 
   constructor(extScr: HTMLCanvasElement) {
-    this._canvasScreen = new CanvasScreen(Game.Info.Width, Game.Info.Height, extScr.getContext("2d"));
+    this._canvasScreen = new CanvasScreen(
+      Game.Info.Width,
+      Game.Info.Height,
+      extScr.getContext("2d"),
+      Game.Info.DispWidth
+    );
     this._input = new InputAIO();
     this._deltaTime = new DeltaTime();
   }
@@ -31,11 +37,25 @@ export class Game {
       }
     }
   }
+  private _tmp = 0;
   private main(): void {
     this._deltaTime.Update("MainStart");
     this._canvasScreen.clearBuffer();
 
-    console.log(this._deltaTime.GetDelta("LoopStart").toFixed(1));
+    // console.log(this._deltaTime.GetDelta("LoopStart").toFixed(1));
+
+    const ctx = this._canvasScreen.Buffer;
+    ctx.fillStyle = "#fff";
+    this._tmp -= 1;
+    this._tmp = this._tmp % 0b100000000 & 0b011111111;
+    ctx.fillRect(this._tmp - 1, 0, 2, Game.Info.Height);
+
+    for (let y = 0; y < Game.Info.Height / 16; y++) {
+      ctx.fillRect(0, y * 16 + 8, Game.Info.Width, 1);
+    }
+    for (let x = 0; x < Game.Info.Width / 16; x++) {
+      ctx.fillRect(x * 16 + 8, 0, 1, Game.Info.Height);
+    }
 
     this._canvasScreen.updateDisp();
   }
